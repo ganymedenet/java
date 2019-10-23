b5d165eafcb882ae3f4a6014f0537fee9b259e8c
ganynmednet <Bsdjh381%>
2019-10-23 23:48:38 +0100

    commit

--- src/check.java
+++ src/check.java
@@ -1,28 +1,17 @@
-import java.util.concurrent.TimeUnit;
-
 public class check {
     public static void main(String args[]){
 
-
         long startTime = System.currentTimeMillis();
 
-        int x = 1;
         int i = 1;
 
-        while (i < 99999){
-            x = 999 * 999;
+        while (i < 99999999){
+            Math.random();
             i++;
-            //System.out.println(x);
         }
 
-        long endTime = System.currentTimeMillis();
-        long duration = (endTime - startTime);
-        //long xxx = TimeUnit.MILLISECONDS.toSeconds(duration);
-        //System.out.format("%.10f", duration);
-        //float b = duration/1000;
-        System.out.println(duration);
-
-
-
-
-}}
+        System.out.println("Math.random() Time: " + (System.currentTimeMillis() - startTime + "ms"));
+        //System.out.println(Math.random());
+        //System.out.println(c);
+}
+}
