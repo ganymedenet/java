49703971f7889e0a276cfdb583e0c75835801d32
ganynmednet <Bsdjh381%>
2019-10-23 22:24:46 +0100

    Initial commit

---
+++ src/check.java
@@ -0,0 +1,11 @@
+public class check {
+
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+
+
+}
